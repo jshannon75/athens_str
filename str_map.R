9048f863aa9c9239a79ab4fac9b8ed3875e92979
Jerry Shannon <jshannon13@gmail.com>
2024-07-03 09:33:51 -0400

    revision 1

--- str_map.R
+++ str_map.R
@@ -1,29 +1,47 @@
 library(tidyverse)
 library(sf)
 library(tidygeocoder)
+sf_use_s2(FALSE)
 
-str<-read_csv("strs_athens.csv")
-parcels<-st_read("C:/Users/jshannon/Dropbox/Jschool/GIS data/Georgia/Athens data/Parcelcentroid_alldata_sqft_bedbath_disttract.gpkg") %>%
-  bind_cols(data.frame(st_coordinates(.)))
+
+str<-read_csv("strs_athens.csv")%>% 
+  mutate(PAR_ADD=parcel_add,
+         parcel_num=if_else(is.na(parcel_num),parcel1,parcel_num)) %>%
+  select(parcel_num,PAR_ADD) %>%
+  filter(is.na(PAR_ADD)==FALSE) %>%
+  mutate(PAR_ADD=if_else(PAR_ADD="220 DR MARTIN LUTHER KING JR",
+                         "220 DR MARTIN LUTHER KING JR PKWY",
+                         PAR_ADD))
+
+
+parcels<-st_read("ACC_Parcels.shp") %>% 
+  st_transform(4326) %>%
+  st_centroid() %>%
+  bind_cols(data.frame(st_coordinates(.))) %>%
+  st_set_geometry(NULL) %>%
+  select(PARCEL_NO,PAR_ADD,X,Y) %>%
+  distinct()
 
 parcel_join<-parcels %>%
-  mutate(parcel_num1=str_replace_all(PARCEL_NO, fixed(" "), "")) %>%
-  inner_join(str %>% 
-               mutate(parcel_num1=str_replace_all(parcel_num, fixed(" "), "")))
+  mutate(parcel_num=PARCEL_NO) %>%
+  inner_join(str) %>%
+  distinct()
+
+parcels_addonly<-parcels %>%
+  group_by(PAR_ADD) %>%
+  summarise(X=mean(X),
+            Y=mean(Y))
 
 str_non<-str %>%
   anti_join(parcel_join) %>%
-  mutate(parcel_add1=paste(parcel_add,", Athens, GA",sep="")) %>%
-  geocode(address=parcel_add1,method='arcgis')
+  rename(parcel_num1=parcel_num) %>%
+  left_join(parcels_addonly) %>%
+  distinct()
 
 str_all<-parcel_join %>%
   anti_join(str_non) %>%
-  bind_rows(str_non %>%
-              rename(X=long,Y=lat) %>%
-              select(parcel1,parcel_add,X,Y) %>%
-              rename(parcel_num=parcel1)) %>%
-  select(parcel_num,parcel_add,X,Y) %>%
-  st_set_geometry(NULL)
+  bind_rows(str_non) %>%
+  select(-parcel_num1) 
 
 write_csv(str_all,"athens_strparcels.csv")
 str_sf<-str_all %>%
